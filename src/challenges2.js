1cc52e73af4ae1539d0e2063b4d2d2429f5d6481
mariananogueirab <mariananbp@gmail.com>
2021-05-25 20:53:08 -0300

    Adiciona requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,10 +15,46 @@ function techList(technologies, name) {
 }
 }
 
-
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function checkNumber(phone) {
+  let count = 0;
+  for (let index = 0; index < phone.length; index +=1 ) {
+    for (let key in phone) {
+        if (phone[key] === phone[index]) {
+          count += 1;
+          if (count === 3 || count > 3) {
+            return 'não é possível gerar um número de telefone com esses valores';
+          }
+        }
+    }
+    count = 0;
+    if (phone[index] < 0 || phone[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  return 'Ok'
+}
+
+function generatePhoneNumber(phone) {
+  let check = checkNumber(phone);
+  let ddd = '';
+  let phone1 = '';
+  let phone2 = '';
+  if (phone.length !== 11) {
+    return 'Array com tamanho incorreto.'
+  } else if (check === 'Ok') {
+    for (let index = 0; index < 2; index += 1) {
+      ddd = ddd + phone[index];
+    }
+    for (let index1 = 2; index1 < 7; index1 += 1) {
+      phone1 = phone1 + phone[index1];
+    }
+    for (let index2 = 7; index2 < phone.length; index2 += 1) {
+      phone2 = phone2 + phone[index2];
+    }
+    return '(' + ddd + ') ' + phone1 + '-' + phone2;
+  }
+  return 'não é possível gerar um número de telefone com esses valores';
 }
 
 // Desafio 12
