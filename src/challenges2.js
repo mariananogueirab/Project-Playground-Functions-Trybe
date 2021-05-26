40c65fa7431e25faf46019ebbbaa06d6e8e1f908
mariananogueirab <mariananbp@gmail.com>
2021-05-26 11:28:50 -0300

    Arrumando lint do desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -2,17 +2,16 @@
 function techList(technologies, name) {
   let technologiesSort = technologies.sort();
   let techObjects = [];
-  if (technologies.length !== 0) {  
+  if (technologies.length !== 0) {
     for (let index = 0; index < technologiesSort.length; index += 1) {
-    techObjects[index] = {
-      tech: technologiesSort[index],
-      name: name
-    };
+      techObjects[index] = {
+        tech: technologiesSort[index],
+        name: name,
+      };
+    }
+    return techObjects;
   }
-  return techObjects;
-} else {
-  return 'Vazio!';
-}
+    return 'Vazio!';
 }
 
 // Desafio 11
