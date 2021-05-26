3f3fb247af93cd8c1a9995ca1700188ae301ffc4
mariananogueirab <mariananbp@gmail.com>
2021-05-26 12:31:34 -0300

    Refatorando desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -5,8 +5,8 @@ function techList(technologies, name) {
   if (technologies.length !== 0) {
     for (let index = 0; index < technologiesSort.length; index += 1) {
       techObjects[index] = {
-        'tech': technologiesSort[index],
-        'name': name,
+        tech: technologiesSort[index],
+        name: name,
       };
     }
     return techObjects;
@@ -18,7 +18,7 @@ function techList(technologies, name) {
 function checkNumber(phone) {
   let count = 0;
   const warning = 'não é possível gerar um número de telefone com esses valores';
-  for (let index = 0; index < phone.length; index += 1 ) {
+  for (let index = 0; index < phone.length; index += 1) {
     for (let key in phone) {
       if (phone[key] === phone[index]) {
         count += 1;
@@ -45,13 +45,13 @@ function generatePhoneNumber(phone) {
   }
   if (check === 'Ok') {
     for (let index = 0; index < 2; index += 1) {
-      ddd = ddd + phone[index];
+      ddd += phone[index];
     }
     for (let index1 = 2; index1 < 7; index1 += 1) {
-      phone1 = phone1 + phone[index1];
+      phone1 += phone[index1];
     }
     for (let index2 = 7; index2 < phone.length; index2 += 1) {
-      phone2 = phone2 + phone[index2];
+      phone2 += phone[index2];
     }
     return '(' + ddd + ') ' + phone1 + '-' + phone2;
   }
@@ -60,21 +60,10 @@ function generatePhoneNumber(phone) {
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
-  let resultA = false;
-  let resultB = false;
-  let resultC = false;
-  if (lineA  < lineB + lineC && lineA > Math.abs(lineB - lineC)) {
-    resultA = true;
-  } else if (lineB < lineA + lineC && lineB > Math.abs(lineA - lineC)) {
-    resultB = true;
-  } else if (lineC < lineA + lineB && lineC > Math.abs(lineA - lineB)) {
-    resultC = true;
-  }
-  if (resultA === resultB === resultC === true) {
+  if (lineA < lineB + lineC && lineA > Math.abs(lineB - lineC) && lineB < lineA + lineC && lineB > Math.abs(lineA - lineC) && lineC < lineA + lineB && lineC > Math.abs(lineA - lineB)) {
     return true;
-  } else {
+  } 
     return false;
-  }
 }
 
 // Desafio 13
